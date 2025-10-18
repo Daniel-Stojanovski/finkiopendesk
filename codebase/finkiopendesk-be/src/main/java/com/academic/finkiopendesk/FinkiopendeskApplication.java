74e510a3cc51993dd3b0b8a64f494c149245fddf
DanielStojanovski <stojanovski1_daniel@hotmail.com>
2025-10-18 17:31:13 +0200

    initial commit

---
+++ codebase/finkiopendesk-be/src/main/java/com/academic/finkiopendesk/FinkiopendeskApplication.java
@@ -0,0 +1,13 @@
+package com.academic.finkiopendesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinkiopendeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinkiopendeskApplication.class, args);
+	}
+
+}
---
+++ codebase/finkiopendesk-be/src/test/java/com/academic/finkiopendesk/FinkiopendeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academic.finkiopendesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinkiopendeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
