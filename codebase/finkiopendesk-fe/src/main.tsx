f86869588fdc68f955d74e705e171913c5903f6a
DanielStojanovski <stojanovski1_daniel@hotmail.com>
2025-10-21 12:30:13 +0200

    task/fe-uiux-base-styling config global style & variables, media breakpoints, color theme

--- codebase/finkiopendesk-fe/src/main.tsx
+++ codebase/finkiopendesk-fe/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 
+import './shared/global.scss'
+
+// dark theme toggle
+// document.documentElement.setAttribute("data-theme", "dark");
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
