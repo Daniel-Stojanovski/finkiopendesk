45b3a4ca4a32c3ed60176eac35bb0c8d4ad45361
DanielStojanovski <stojanovski1_daniel@hotmail.com>
2026-01-27 19:00:36 +0100

    fe-uiux-channels-sidebar v1.0 implement channels sidebar, add to subject discussion view.

--- codebase/finkiopendesk-fe/src/pages/views/discussion/SubjectDiscussion.tsx
+++ codebase/finkiopendesk-fe/src/pages/views/discussion/SubjectDiscussion.tsx
@@ -2,22 +2,32 @@ import { useEffect, useState } from "react";
 import api from "../../../shared/axios";
 import '../views.scss';
 import {useParams} from "react-router-dom";
+import ChannelSidebar from "../../../components/blocks/ChannelSidebar/ChannelSidebar";
 
 const SubjectDiscussion = () => {
     const { id } = useParams();
     const [discussion, setDiscussion] = useState(null);
+    const [channels, setChannels] = useState([]);
 
     useEffect(() => {
         if (!id) return;
         api.get(`/subjects/sid/${id}`).then(response => setDiscussion(response.data));
     }, [id]);
 
+    useEffect(() => {
+        api.get(`/subjects/channels/sid/${id}/active`).then(response => setChannels(response.data));
+    }, []);
+
     return (
-        <div className="discussion-view">
-            <h3>{discussion?.name}</h3>
-            {discussion?.description && (
-                <p>{discussion.description}</p>
-            )}
+        <div id="subject-discussion">
+            <div className="discussion-view">
+                <h3>{discussion?.name}</h3>
+                {discussion?.description && (
+                    <p>{discussion.description}</p>
+                )}
+            </div>
+
+            <ChannelSidebar channels={channels}/>
         </div>
     );
 }
